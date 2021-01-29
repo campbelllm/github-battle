ab57ebea5caebbac0a9f65fbc8d48edda15c4314
Lindsey Smith <campbelllm@gmail.com>
2021-01-29 10:31:14 -0700

    start of file structure and ReactDOM

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  // React Element
+  // Where to render the Element to
+  <App/>,
+  document.getElementById('app')
+)
